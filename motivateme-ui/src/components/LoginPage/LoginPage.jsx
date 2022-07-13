e394dd02f71a6a97dc1d9876ede2c0d6dfccc3e0
StaticMaster <gnaboys@gmail.com>
2022-07-13 13:20:21 -0400

    setup basic login page

--- motivateme-ui/src/components/LoginPage/LoginPage.jsx
+++ motivateme-ui/src/components/LoginPage/LoginPage.jsx
@@ -3,6 +3,31 @@ import "./LoginPage.css"
 
 export default function LoginPage() {
   return (
-    <div className=''>LoginPage</div>
+    
+    <div className='login-page'>
+     <div className='side-nav'>
+     
+
+     </div>
+      <h2 className='login-title'>Login</h2>
+      <br></br>
+      <h3 className='login-text'>Track your progress with friends!</h3>
+     <div className='login-form'>
+     <form>
+          <label for="email" className='label'>Email</label>
+          <br></br>
+          <input type="text" id="email" name="user@gmail.com" className='form-input' placeholder='Type your email'></input>
+          <br></br>
+          <label for="password" className='label'>Password</label>
+          <br></br>
+          <input type="text" id="password" name="password" className='form-input' placeholder='Type your password'></input>
+          <br></br>
+          <input type="checkbox" id="remember" name="remember" value="remember"></input>
+
+          <button type="button" className='login-button'>Login</button>
+    </form>
+
+    </div>
+    </div>
   )
 }
