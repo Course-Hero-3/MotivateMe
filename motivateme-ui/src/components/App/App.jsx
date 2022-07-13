659cd44ea5f326bcde4a5e66af04738bbcc9c36c
stephane mbenga <mbengastephane34@gmail.com>
2022-07-13 13:19:07 -0400

    setup basic React Router routes

--- motivateme-ui/src/components/App/App.jsx
+++ motivateme-ui/src/components/App/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from "../LoginPage/LoginPage"
+import RegisterPage from "../RegisterPage/RegisterPage"
+import Navbar from "../Navbar/Navbar"
+import TodoPage from "../TodoPage/TodoPage"
+import LandingPage from "../LandingPage/LandingPage"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+          <div className="App">
+            <Navbar/> 
+            <LandingPage/>
+            <Routes>
+                <Route path = "/login" element = {LoginPage}/>
+                <Route path = "/register" element = {RegisterPage}/>
+                <Route path = "/ToDo" element = {TodoPage}/>
+            </Routes>
+          </div>
+      </BrowserRouter>
+    </React.Fragment>
   )
 }
 
