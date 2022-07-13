d1e2e57d22912cea1982bf203dcb37d579353971
stephane mbenga <mbengastephane34@gmail.com>
2022-07-13 13:04:35 -0400

    created/initalized api client file

---
+++ motivateme-ui/services/apiclient.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+class ApiClient {
+    constructor(remotehosturl){
+        this.token = ""
+        this.remotehosturl = remotehosturl
+    }
+
+}
+
+
+export default ApiClient
