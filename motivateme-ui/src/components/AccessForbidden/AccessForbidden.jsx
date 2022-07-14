96cf47ce853d51a0c174f6acaf4c0245e58429ee
StaticMaster <gnaboys@gmail.com>
2022-07-14 10:23:43 -0400

    added some css to accessforbidden component

--- motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
+++ motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./AccessForbidden.css"
+import Halt  from "../../assets/halt.mp4" 
 
 export default function AccessForbidden() {
   return (
     <div className='accessforbidden'>
-
-
-        
+      <h1>ACCESS FORBIDDEN</h1>
+    <img src={Halt} classname ="gif"/>
+    <br></br>
+    <div className='statment'>
+    <h3> You could make an account to see this.Click</h3>
+    <a href='/register'>here.</a>
+     </div>   
     </div>
   )
 }
