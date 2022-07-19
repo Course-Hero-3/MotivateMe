b6db200d4b284ce6e64fa32608c0f73484ef2e0a
stephane mbenga <mbengastephane34@gmail.com>
2022-07-19 00:27:17 -0400

    implemented login feature

--- motivateme-ui/src/components/LoginPage/LoginPage.jsx
+++ motivateme-ui/src/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import "./LoginPage.css"
-
+import ApiClient
+ from '../../../services/apiclient'
 export default function LoginPage() {
+  const [loginForm, setLoginForm] = useState({email:"", password:""})
+  const [loginError, setLoginError] = useState(null)
+
+  const handleOnLoginFormChange = (event) => {
+    setLoginForm({...loginForm, [event.target.name]:event.target.value})
+    console.log(loginForm)
+  }
+  const apiClient = new ApiClient("http://localhost:3001")
+  const handleOnLoginFormSubmit = async () => {
+    let {data, error} = await apiClient.login(loginForm)
+    
+    if (data) {
+      console.log(data)
+      apiClient.setToken(data.token)
+      setLoginForm({ email:"", password:""})
+    } else {setLoginError(error)}
+  }
   return (
     
     <div className='login-page'>
@@ -16,17 +35,17 @@ export default function LoginPage() {
      <form>
           <label for="email" className='label'>Email</label>
           <br></br>
-          <input type="text" id="email" name="user@gmail.com" className='form-input' placeholder='Type your email'></input>
+          <input type="text" id="email" name="email" className='form-input' placeholder='Type your email' value = {loginForm.email} onChange = {handleOnLoginFormChange}></input>
           <br></br>
           <label for="password" className='label'>Password</label>
           <br></br>
-          <input type="text" id="password" name="password" className='form-input' placeholder='Type your password'></input>
+          <input type="text" id="password" name="password" className='form-input' placeholder='Type your password' value = {loginForm.password} onChange = {handleOnLoginFormChange}></input>
           <br></br>
           <div className='checkbox'>
           <input type="checkbox" id="remember" name="remember" value="remember"></input>
           <p className='checkbox-text'>Remember me</p>
           </div>
-          <button type="button" className='login-button'>Login</button>
+          <button type="button" className='login-button' onClick={handleOnLoginFormSubmit}>Login</button>
           <br></br>
           <div className='footer'>
           <p className='footer-text'>Don't have an account? Sign up<a href='/register'> here</a></p>
