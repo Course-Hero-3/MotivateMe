4886c581d8be22755384546ec71291a7fd65aa51
kfr7 <kianranjbar7@gmail.com>
2022-07-21 10:38:30 -0700

    Updated and added graphs/statistics

--- motivateme-ui/src/components/BarVisual/BarVisual.jsx
+++ motivateme-ui/src/components/BarVisual/BarVisual.jsx
@@ -1,38 +1,37 @@
+import React from "react";
+import "./BarVisual.css";
+import { Bar } from "react-chartjs-2";
 
-import React from 'react'
-import "./BarVisual.css"
-import { Bar } from "react-chartjs-2"
-
-    
-export default function BarVisual( { labels, label, actualData, colors } ) {
-    // take in these 3 props when we render all visuals
+export default function BarVisual({ labels, label, actualData, colors }) {
+  // take in these 3 props when we render all visuals
   return (
-        <Bar 
-        data={{
-            labels: labels,
-            datasets: [{
-                label: label,
-                data: actualData,
-                backgroundColor: colors[0],
-                borderColor: colors[1],
-                borderWidth: 1
-            }]
-        }}
-        options={{
-            plugins: {
-                legend: {
-                display: false,
-              }
-            },
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }}
-        height={400}
-        width={600}
-        />
-  )
+    <Bar
+      data={{
+        labels: labels,
+        datasets: [
+          {
+            label: label,
+            data: actualData,
+            backgroundColor: colors[0],
+            borderColor: colors[1],
+            borderWidth: 1,
+          },
+        ],
+      }}
+      options={{
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      }}
+      height={400}
+      width={600}
+    />
+  );
 }
-
