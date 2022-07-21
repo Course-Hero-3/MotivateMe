b9c5b34541a2fe2c15b0857a52c05b73704b7620
kfr7 <kianranjbar7@gmail.com>
2022-07-20 22:23:10 -0700

    Fixed to do list input forms and error checking

--- motivateme-ui/src/components/TodoForm/TodoForm.jsx
+++ motivateme-ui/src/components/TodoForm/TodoForm.jsx
@@ -2,24 +2,87 @@ import React from 'react'
 import { useState } from 'react'
 import "./TodoForm.css"
 import apiClient from '../../../services/apiclient'
-export default function TodoForm({formType, setUpdateOrComplete, modalSelected, showModal, updateForm, completeForm, setUpdateForm, taskId, setCompleteForm, handleOnCompleteFormChange, handleOnUpdateFormChange, handleOnCompleteSubmit, handleOnUpdateSubmit, originalForm, handleOnDeleteTask
+export default function TodoForm({formType, 
+                                  setUpdateOrComplete, 
+                                  taskError, 
+                                  showModal, 
+                                  updateForm, 
+                                  completeForm, 
+                                  setUpdateForm, 
+                                  taskId, 
+                                  setCompleteForm, 
+                                  handleOnCompleteFormChange, 
+                                  handleOnUpdateFormChange, 
+                                  handleOnCompleteSubmit, 
+                                  handleOnUpdateSubmit, 
+                                  originalForm, 
+                                  handleOnDeleteTask,
+                                  name
 }) {
   const [createForm, setCreateForm] = useState({name:"", description: "", category:"", dueDate:"", dueTime:""})
   const [createError, setCreateError] = useState(null)
 
   const handleOnCreateFormChange = (event) => {
+
+    if (createError === "Name field is mandatory" && 
+        event.target.name === "name" && event.target.value.length > 0) {
+          setCreateError(null)
+    }
+    if (createError === "Category field is mandatory" && 
+        event.target.name === "category" && event.target.value.length > 0) {
+          setCreateError(null)
+    }
+    if (createError === "Due Date field is mandatory" && 
+        event.target.name === "dueDate" && event.target.value.length > 0) {
+          setCreateError(null)
+    }
+    if (createError === "Due Time field is mandatory" && 
+        event.target.name === "dueTime" && event.target.value.length > 0) {
+          setCreateError(null)
+          
+    }
+    console.log(event)
+
     setCreateForm({ ...createForm, [event.target.name]: event.target.value })
   }
 
   const handleOnCreateSubmit = async (event) => {
-    /**api client call */
     event.preventDefault()
+
+    if (createForm.category.length !== 0 &&
+      createForm.dueDate.length !== 0 &&
+      createForm.name.length !== 0 &&
+      createForm.dueTime.length !== 0) {
+    setCreateError(null)
+    }
+    else {
+      if (createForm.name.length === 0) {
+        setCreateError("Name field is mandatory")
+        return
+      }
+      if (createForm.category.length === 0) {
+        setCreateError("Category field is mandatory")
+        return
+      }
+      if (createForm.dueDate.length === 0) {
+        setCreateError("Due Date field is mandatory")
+        return
+      }
+      if (createForm.dueTime.length === 0) {
+        setCreateError("Due Time field is mandatory")
+        return
+      }
+    }
+    
+
+    if (createError !== null) {
+      return
+    }
 
     let {data, error} = await apiClient.addTask(createForm)
     
     if (data?.task) {
-      console.log("NEW TASK IS: ", data.task)
-      setCreateError("")
+      setCreateError(null)
       setCreateForm({name:"", description: "", category:"", dueDate:"", dueTime:""})
       showModal("")
     } else {setCreateError(error)}
@@ -28,36 +91,46 @@ export default function TodoForm({formType, setUpdateOrComplete, modalSelected, 
   return (
     <div className='todo-form'>
 
-        {formType === "create"?<TodoCreate handleOnCreateSubmit = {handleOnCreateSubmit} 
+        { formType === "create" ? <TodoCreate handleOnCreateSubmit = {handleOnCreateSubmit} 
                                           handleOnCreateFormChange = {handleOnCreateFormChange} 
                                           showModal = {showModal} 
                                           createForm = {createForm} 
-          setCreateForm = {setCreateForm}/>:null}
+                                          setCreateForm = {setCreateForm}
+                                          createError = {createError}/>
+                                : null }
 
-        {formType === "update"?<TodoUpdate showModal = {showModal} 
-                                            updateForm = {updateForm} 
+        {formType === "update"?<TodoUpdate  updateForm = {updateForm} 
                                             setUpdateForm = {setUpdateForm}
                                             handleOnUpdateFormChange = {handleOnUpdateFormChange} 
                                             handleOnUpdateSubmit = {handleOnUpdateSubmit}
                                             setUpdateOrComplete = {setUpdateOrComplete}
                                             originalForm = {originalForm}
                                             handleOnDeleteTask = {handleOnDeleteTask}
-          />:null}
+                                            taskError = {taskError} />
+                                : null}
           
-        {formType === "complete"?<TodoComplete showModal = {showModal} 
+        {formType === "complete"?<TodoComplete 
+                                              name = {name}
+                                              showModal = {showModal} 
                                               completeForm = {completeForm} 
                                               setCompleteForm = {setCompleteForm} 
                                               handleOnCompleteFormChange = {handleOnCompleteFormChange}
                                               handleOnCompleteSubmit = {handleOnCompleteSubmit} 
                                               taskId = {taskId}
                                               setUpdateOrComplete = {setUpdateOrComplete}
+                                              taskError = {taskError}
         />:null}
 
     </div>
   )
 }
 
-export function TodoCreate ({showModal, modalSelected, createForm, setCreateForm, handleOnCreateFormChange, handleOnCreateSubmit}) {
+export function TodoCreate ({showModal, 
+                              createForm, 
+                              setCreateForm,
+                              handleOnCreateFormChange, 
+                              handleOnCreateSubmit,
+                              createError }) {
   return (<div className={`form_modal`}>
 
         <form className='form_modal_content'>
@@ -75,22 +148,45 @@ export function TodoCreate ({showModal, modalSelected, createForm, setCreateForm
              </div>
              <div className='input-field form'>
              <span className='task-name'>Category</span>
-             <input className='form-input form' type ="text" name='category' placeholder='type category' value  = {createForm.category} onChange = {handleOnCreateFormChange}/>
+             <div className="dropdown">
+                <button type="button" className="dropbtn">{createForm.category.length === 0 ? "(Hover to Select)" : createForm.category }</button>
+                <div className="dropdown-content">
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnCreateFormChange({target: { name: "category", value: "test"}})}}>test</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnCreateFormChange({target: { name: "category", value: "project"}})}}>project</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnCreateFormChange({target: { name: "category", value: "homework"}})}}>homework</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnCreateFormChange({target: { name: "category", value: "quiz"}})}}>quiz</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnCreateFormChange({target: { name: "category", value: "essay"}})}}>essay</button>
+                </div>
+              </div>
+             {/* <input className='form-input form' type ="text" name='category' placeholder='type category' value  = {createForm.category} onChange = {handleOnCreateFormChange}/> */}
               
              </div>
              <div className='split-input-field form'>
                 <div className='input-field form-split'>
-                <span className='task-name'>Date</span>
+                <span className='task-name'>Due Date</span>
                 <input className='form-input split' type = "date" name='dueDate' placeholder='type task name' value  = {createForm.dueDate} onChange = {handleOnCreateFormChange}/>
                 </div>
                 <div className='input-field form-split'>
-                <span className='task-name'>Time</span>
+                <span className='task-name'>Due Time</span>
                 <input className='form-input split' type = "time" name='dueTime' placeholder='type task name' value  = {createForm.dueTime} onChange = {handleOnCreateFormChange}/>
                 </div>
               </div>
               <div className='input-field form'>
              <span className='task-name'>Description</span>
              <textarea className='form-input description' type = "text" name='description' placeholder='description...' value  = {createForm.description} onChange = {handleOnCreateFormChange}></textarea>
+             </div>
+             <div>
+               <p className='error-create'>{createError ? createError : ""}</p>
              </div>
              <button className = "form-submit-btn" type='button' onClick = {handleOnCreateSubmit}>Submit</button>
         </div>
@@ -101,8 +197,15 @@ export function TodoCreate ({showModal, modalSelected, createForm, setCreateForm
 }
 
 
-export function TodoUpdate ({showModal, updateForm, setUpdateForm, handleOnUpdateFormChange, handleOnUpdateSubmit,originalForm, setUpdateOrComplete, handleOnDeleteTask
-}) {
+export function TodoUpdate ({ updateForm, 
+                              setUpdateForm, 
+                              handleOnUpdateFormChange, 
+                              handleOnUpdateSubmit,
+                              originalForm, 
+                              setUpdateOrComplete, 
+                              handleOnDeleteTask,
+                              taskError }) {
+
     return (<div className={`form_modal`}>
     <form className='form_modal_content'>
     <div className='form-header'>
@@ -119,16 +222,34 @@ export function TodoUpdate ({showModal, updateForm, setUpdateForm, handleOnUpdat
          </div>
          <div className='input-field form'>
          <span className='task-name'>Edit Category</span>
-         <input className='form-input form' name='category' placeholder='Type category' value = {updateForm.category} onChange = {(event) => {handleOnUpdateFormChange(event, updateForm, setUpdateForm)}}/>
-
+         <div className="dropdown">
+                <button type="button" className="dropbtn">{updateForm.category.length === 0 ? "(Hover to Select)" : updateForm.category }</button>
+                <div className="dropdown-content">
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnUpdateFormChange({target: { name: "category", value: "test"}}, updateForm, setUpdateForm)}}>test/exam</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnUpdateFormChange({target: { name: "category", value: "project"}}, updateForm, setUpdateForm)}}>project</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnUpdateFormChange({target: { name: "category", value: "homework"}}, updateForm, setUpdateForm)}}>homework</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnUpdateFormChange({target: { name: "category", value: "quiz"}}, updateForm, setUpdateForm)}}>quiz</button>
+                  <button type="button" 
+                          className="option-btn"
+                          onClick={() => {handleOnUpdateFormChange({target: { name: "category", value: "essay"}}, updateForm, setUpdateForm)}}>essay</button>
+                </div>
+              </div>
          </div>
          <div className='split-input-field form'>
             <div className='input-field form-split'>
-            <span className='task-name'>Edit Date</span>
+            <span className='task-name'>Edit Due Date</span>
             <input className='form-input split' type = "date" name='dueDate' placeholder='Type task Name' value = {updateForm.dueDate} onChange = {(event) => {handleOnUpdateFormChange(event, updateForm, setUpdateForm)}}/>
             </div>
             <div className='input-field form-split'>
-            <span className='task-name'>Edit Time</span>
+            <span className='task-name'>Edit Due Time</span>
             <input className='form-input split' type = "time" name='dueTime' placeholder='Type task Name' value = {updateForm.dueTime} onChange = {(event) => {handleOnUpdateFormChange(event, updateForm, setUpdateForm)}}/>
             </div>
           </div>
@@ -136,11 +257,13 @@ export function TodoUpdate ({showModal, updateForm, setUpdateForm, handleOnUpdat
          <span className='task-name'>Edit Description</span>
          <textarea className='form-input description' name='description' placeholder='Type category' value = {updateForm.description} onChange = {(event) => {handleOnUpdateFormChange(event, updateForm, setUpdateForm)}}></textarea>
           
-         </div>
+          </div>
+          <div>
+            <p className='error-create'>{taskError ? taskError : ""}</p>
+          </div>
          <div className='update-footer-buttons'>
-            <button className = "form-submit-btn" type='button' onClick = {(event) => {handleOnUpdateSubmit(event, updateForm, setUpdateForm);
-            setUpdateOrComplete(null)}}>Update</button>
-                <button className = "form-delete-btn" type='button' onClick={(event) => {handleOnDeleteTask(event, updateForm.taskId)}}>Delete</button>
+            <button className = "form-submit-btn" type='button' onClick = {(event) => {handleOnUpdateSubmit(event, updateForm, setUpdateOrComplete)}}>Update</button>
+            <button className = "form-delete-btn" type='button' onClick={(event) => {handleOnDeleteTask(event, updateForm.taskId)}}>Delete</button>
          </div>
     
 
@@ -151,7 +274,14 @@ export function TodoUpdate ({showModal, updateForm, setUpdateForm, handleOnUpdat
     )
 }
 
-export function TodoComplete ({setUpdateOrComplete, setCompleteForm, completeForm, handleOnCompleteFormChange,handleOnCompleteSubmit, name, taskId}) {
+export function TodoComplete ({setUpdateOrComplete, 
+                              setCompleteForm, 
+                              completeForm, 
+                              handleOnCompleteFormChange,
+                              handleOnCompleteSubmit, 
+                              name, 
+                              taskId,
+                              taskError }) {
     return (<div className={`form_modal`}>
     <div className='form_modal_content'>
     <div className='form-header'>
@@ -167,32 +297,36 @@ export function TodoComplete ({setUpdateOrComplete, setCompleteForm, completeFor
          </div>
          <div className='input-field form'>
          <span className='task-name'>Score</span>
-         <input className='form-input form' name='score' type = "number" placeholder='enter percent without % symbol' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
+         <input className='form-input form' name='score' type = "number" placeholder='Enter percent without % symbol' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
           
          </div>
          <div className='split-input-field form'>
             <div className='input-field form-split'>
             <span className='task-name'>People worked with</span>
-            <input className='form-input split' name='peopleWith' type = "number" placeholder='how many people did you work with' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
+            <input className='form-input split' name='peopleWith' type = "number" placeholder='People worked with' value={completeForm.peopleWith} onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
             </div>
             <div className='input-field form-split'>
             <span className='task-name'>Time Spent</span>
-            <input className='form-input split' type = "number" name='timeSpent' placeholder='in minutes' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
+            <input className='form-input split' type = "number" name='timeSpent' placeholder='Time in minutes' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
             </div>
           </div>
           <div className='input-field form'>
          <span className='task-name'>Comments</span>
-         <textarea className='form-input description' name='comment' type = "text"placeholder='any comments?' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}></textarea>
+         <textarea className='form-input description' name='comment' type = "text"placeholder='Add any comments' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}></textarea>
           
          </div>
-         <div className='input-field checkbox'>
-         <span className='task-name'>Completed on time</span>
-         <input className='form-input form' type = "checkbox" name='onTime' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
+
+         <div className='text-and-checkbox-area'>
+          <p>Completed On Time?</p>
+          <input className='checkbox-right' type = "checkbox" name='onTime' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
          </div>
-         <div className='input-field checkbox'>
-         <span className='task-name'>Public</span>
-         <input className='form-input form' type = "checkbox" name='public' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
+         <div className='text-and-checkbox-area'>
+          <p>Public?</p>
+          <input className='checkbox-right' type = "checkbox" name='public' onChange = {(event) => {handleOnCompleteFormChange(event, completeForm, setCompleteForm)}}/>
          </div>
+         <div>
+            <p className='error-create'>{taskError ? taskError : ""}</p>
+          </div>
          <button className = "form-submit-btn" type='button' onClick = {(event) => {handleOnCompleteSubmit(event, completeForm)}}>Complete</button>
 
     </div>
