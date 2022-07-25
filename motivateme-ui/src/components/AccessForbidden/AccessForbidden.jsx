4c3eab5be45a066c357d6c7da4a6ba39063abee9
Kian Ranjbar <79958814+kfr7@users.noreply.github.com>
2022-07-25 15:00:14 -0700

    Got rid of the console.log

--- motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
+++ motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -4,7 +4,6 @@ import {Link} from "react-router-dom"
 
 export default function AccessForbidden( { setCurrPage } ) {
   React.useEffect(() => {
-    console.log("GOING TO BE LANDING")
     setCurrPage("landing")
 }, [])
 
