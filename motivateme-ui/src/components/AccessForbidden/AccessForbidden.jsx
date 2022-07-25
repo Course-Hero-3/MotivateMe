b3854cdf44ca314b72f943044be86b3f7b789983
kfr7 <kianranjbar7@gmail.com>
2022-07-25 14:46:39 -0700

    Fixed access forbidden. Changed it from a path it navigates to, to a conditionally rendered component

--- motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
+++ motivateme-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 
 export default function AccessForbidden( { setCurrPage } ) {
   React.useEffect(() => {
+    console.log("GOING TO BE LANDING")
     setCurrPage("landing")
 }, [])
 
--- motivateme-ui/src/components/SocialPage/SocialPage.jsx
+++ motivateme-ui/src/components/SocialPage/SocialPage.jsx
@@ -3,55 +3,61 @@ import { useNavigate } from "react-router-dom";
 import "./SocialPage.css";
 import apiClient from "../../../services/apiclient";
 import { useState } from "react";
+import AccessForbidden from "../AccessForbidden/AccessForbidden";
 
 export default function SocialPage({ user, setCurrPage }) {
   const [rightTabState, setRightTabState] = useState("friends");
   const navigate = useNavigate();
   React.useEffect(() => {
     // if user is not logged in, redirect to access forbidden page
-    const checkUserLoggedIn = async () => {
-      const { error } = await apiClient.fetchUserFromToken();
-      if (error) navigate("/accessforbidden");
-    };
 
-    checkUserLoggedIn();
     //otherwise, set the curr page to "social"
-    setCurrPage("social");
+    if (user !== undefined && user !== null) {
+      setCurrPage("social");
+    }
   });
 
   return (
-    <div className="social-page">
-      <h1 className="main-title">Social Feed</h1>
-      <div className="social-content">
-        <div className="activity-feed">
-          <h4>Activity Title</h4>
-          <div className="main-feed">activity card</div>
-        </div>
-        <div className="friends-content">
-          <div className="buttons">
-            <button type="button" className="button">
-              Explore
-            </button>
-            <button type="button" className="button">
-              Friends
-            </button>
-          </div>
-          <div className="friend-section">
-            {rightTabState === "friends" ? (
-              <h2 className="friend-text">Friends List</h2>
-            ) : (
-              <h2 className="friend-text">The World</h2>
-            )}
-            <input
-              type="search"
-              className="search"
-              placeholder="search"
-            ></input>
-          </div>
-        </div>
-      </div>
+    <>
+      {user !== null && user !== undefined ? (
+        <>
+          <div className="social-page">
+            <h1 className="main-title">Social Feed</h1>
+            <div className="social-content">
+              <div className="activity-feed">
+                <h4>Activity Title</h4>
+                <div className="main-feed">activity card</div>
+              </div>
+              <div className="friends-content">
+                <div className="buttons">
+                  <button type="button" className="button">
+                    Explore
+                  </button>
+                  <button type="button" className="button">
+                    Friends
+                  </button>
+                </div>
+                <div className="friend-section">
+                  {rightTabState === "friends" ? (
+                    <h2 className="friend-text">Friends List</h2>
+                  ) : (
+                    <h2 className="friend-text">The World</h2>
+                  )}
+                  <input
+                    type="search"
+                    className="search"
+                    placeholder="search"
+                  ></input>
+                </div>
+              </div>
+            </div>
 
-      <div className="main-feed"></div>
-    </div>
+            <div className="main-feed"></div>
+          </div>
+        </>
+      ) : (
+        <AccessForbidden setCurrPage={setCurrPage} />
+      )}
+    </>
   );
 }
