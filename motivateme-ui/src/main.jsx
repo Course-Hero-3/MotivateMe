a25e65191ae1e5c61bb358e7b1ea81a57d2ff2df
stephane mbenga <mbengastephane34@gmail.com>
2022-08-04 16:31:51 -0400

    fixed files

--- motivateme-ui/src/main.jsx
+++ motivateme-ui/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
-import ('typeface-assistant')
 import {ChakraProvider} from '@chakra-ui/react'
 import theme from "./components/theme"
 
