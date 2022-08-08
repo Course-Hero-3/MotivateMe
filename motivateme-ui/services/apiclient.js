f094fe048aaf08f2b8e66c2916bf8fe7600ba451
kfr7 <kianranjbar7@gmail.com>
2022-08-07 19:06:18 -0700

    Changed backend link from localhost to heroku link

--- motivateme-ui/services/apiclient.js
+++ motivateme-ui/services/apiclient.js
@@ -113,4 +113,4 @@ class ApiClient {
 }
 
 // when deploying to heroku may have to change!!!
-export default new ApiClient("http://localhost:3001")
+export default new ApiClient("https://motivateme-project.herokuapp.com")
